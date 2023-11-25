550390a81e6d7a66400ed3ec28b237862bc74566
AC_Marvel <gil_1_10@hotmail.com>
2023-11-25 23:42:46 +0100

    Conclusão deste projeto.

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -3,11 +3,16 @@ const key = "35793bceae0cc9b1f8af44ab2c712d15"
 
 
 function colocarDadosNaTela(dados){
-    document.querySelector(".cidade").innerHTML = dados.name
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em "+dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp)+"°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".humidade").innerHTML = dados.main.humidity+"%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
 async function buscarCidade(cidade){
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt `).then(resposta => resposta.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt&units=metric`).then(resposta => resposta.json())
     
     colocarDadosNaTela(dados)
 }
